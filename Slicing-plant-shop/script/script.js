837d40f4ac45603ab5ae80e56f35ac0e2470ec18
Djdaan Truong <daantd.a12017@gmail.com>
2022-05-07 05:04:33 +0700

    home section

--- Slicing-plant-shop/script/script.js
+++ Slicing-plant-shop/script/script.js
@@ -7,3 +7,14 @@ $(document).ready(() => {
     $('.menu-wrapper').removeClass('active')
   })
 })
+
+var swiper = new Swiper('.mySwiperHome', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 2000,
+  },
+})
